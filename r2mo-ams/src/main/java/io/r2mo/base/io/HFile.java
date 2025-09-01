91bdc3f1f4cdfba8adaf08087430279e8791f512
Lang <silentbalanceyh@126.com>
2025-09-01 09:49:10 +0800

    Rapid Extension for New Module

--- r2mo-ams/src/main/java/io/r2mo/base/io/HFile.java
+++ r2mo-ams/src/main/java/io/r2mo/base/io/HFile.java
@@ -1,6 +1,8 @@
 package io.r2mo.base.io;
 
 /**
+ * 文件操作
+ *
  * @author lang : 2025-08-28
  */
 public interface HFile {
--- r2mo-ams/src/main/java/io/r2mo/spi/FactoryIo.java
+++ r2mo-ams/src/main/java/io/r2mo/spi/FactoryIo.java
@@ -1,11 +1,11 @@
 package io.r2mo.spi;
 
-import io.r2mo.base.io.HStore;
+import io.r2mo.base.io.HStorage;
 
 /**
  * @author lang : 2025-08-28
  */
 public interface FactoryIo {
 
-    HStore ioStore();
+    HStorage ioStore();
 }
