3b53c962b74ce85f68d9f8367f835f9ff35e9f4a
Lang <silentbalanceyh@126.com>
2025-09-09 23:48:01 +0800

    Attachment

--- r2mo-spring-excel/src/main/java/io/r2mo/spring/excel/spi/ExcelFactory.java
+++ r2mo-spring-excel/src/main/java/io/r2mo/spring/excel/spi/ExcelFactory.java
@@ -3,11 +3,13 @@ package io.r2mo.spring.excel.spi;
 import io.r2mo.spring.common.webflow.attachment.FactoryMultipart;
 import io.r2mo.spring.common.webflow.attachment.MultipartMeta;
 import io.r2mo.spring.common.webflow.attachment.MultipartProcessor;
+import io.r2mo.typed.annotation.SPID;
 import io.r2mo.typed.cc.Cc;
 
 /**
  * @author lang : 2025-09-09
  */
+@SPID(FactoryMultipart.SPID_DEFAULT_MULTIPART)
 public class ExcelFactory implements FactoryMultipart {
 
     private final Cc<String, MultipartProcessor<?>> CCT_PROCESSOR = Cc.openThread();
@@ -15,7 +17,7 @@ public class ExcelFactory implements FactoryMultipart {
     @Override
     @SuppressWarnings("unchecked")
     public <T> MultipartProcessor<T> mediaProcessor(final Class<T> cls) {
-        return (MultipartProcessor<T>) CCT_PROCESSOR.pick(() -> new ExcelProcessor<>(cls), cls.getName());
+        return (MultipartProcessor<T>) this.CCT_PROCESSOR.pick(() -> new ExcelProcessor<>(cls), cls.getName());
     }
 
     @Override
