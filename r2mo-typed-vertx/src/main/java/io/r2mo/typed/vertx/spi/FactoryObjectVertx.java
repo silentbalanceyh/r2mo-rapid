544a698d5a8d62ecc57a5552e0b17bfa50a553c6
Lang <silentbalanceyh@126.com>
2025-10-06 01:16:21 +0800

    Vertx.yml

--- r2mo-typed-vertx/src/main/java/io/r2mo/typed/vertx/spi/FactoryObjectVertx.java
+++ r2mo-typed-vertx/src/main/java/io/r2mo/typed/vertx/spi/FactoryObjectVertx.java
@@ -1,9 +1,14 @@
 package io.r2mo.typed.vertx.spi;
 
+import cn.hutool.core.util.StrUtil;
 import io.r2mo.spi.FactoryObject;
 import io.r2mo.typed.json.JArray;
 import io.r2mo.typed.json.JObject;
 import io.r2mo.typed.json.JUtil;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+import java.util.Objects;
 
 /**
  * Vertx 实现类
@@ -18,31 +23,49 @@ public class FactoryObjectVertx implements FactoryObject {
 
     @Override
     public JObject jsonObject(final Object wrapType) {
-        return null;
+        if (Objects.isNull(wrapType)) {
+            return new JObjectVertx();
+        }
+        if (wrapType instanceof final JsonObject wrapJ) {
+            return new JObjectVertx(wrapJ);
+        }
+        return new JObjectVertx();
     }
 
     @Override
     public JObject jsonObject(final String jsonStr) {
-        return null;
+        if (StrUtil.isBlank(jsonStr)) {
+            return new JObjectVertx();
+        }
+        return new JObjectVertx(jsonStr);
     }
 
     @Override
     public JArray jsonArray() {
-        return null;
+        return new JArrayVertx();
     }
 
     @Override
     public JArray jsonArray(final Object wrapType) {
-        return null;
+        if (Objects.isNull(wrapType)) {
+            return new JArrayVertx();
+        }
+        if (wrapType instanceof final JsonArray wrapA) {
+            return new JArrayVertx(wrapA);
+        }
+        return new JArrayVertx();
     }
 
     @Override
     public JArray jsonArray(final String jsonStr) {
-        return null;
+        if (StrUtil.isBlank(jsonStr)) {
+            return new JArrayVertx();
+        }
+        return new JArrayVertx(jsonStr);
     }
 
     @Override
     public JUtil jsonUtil() {
-        return null;
+        return JUtilVertx.getInstance();
     }
 }
