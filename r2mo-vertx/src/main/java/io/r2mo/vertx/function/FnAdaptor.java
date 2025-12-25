6b25fc6bf124c94e06d9d1a19e665348158bc608
Lang <silentbalanceyh@126.com>
2025-12-25 16:45:18 +0800

    Util API

--- r2mo-vertx/src/main/java/io/r2mo/vertx/function/FnAdaptor.java
+++ r2mo-vertx/src/main/java/io/r2mo/vertx/function/FnAdaptor.java
@@ -5,20 +5,25 @@ import io.r2mo.typed.exception.WebException;
 import io.r2mo.typed.webflow.WebState;
 import io.vertx.core.json.JsonObject;
 
+import java.util.Objects;
+
 /**
  * @author lang : 2025-09-30
  */
 class FnAdaptor {
 
-    static JsonObject adapt(final WebException error) {
+    static JsonObject failJson(final WebException error) {
         final JsonObject failure = new JsonObject();
         final WebState status = error.getStatus();
         final HttpResponseStatus statusValue = status.value();
-        failure.put("reason", statusValue.reasonPhrase());
-        failure.put("status", statusValue.code());
-        failure.put("message", error.getMessage());
-        failure.put("code", error.getCode());
-        failure.put("info", error.getInfo());
+        failure.put("reason", statusValue.reasonPhrase());      // HTTP 原始原因
+        failure.put("status", statusValue.code());              // HTTP 状态码
+        failure.put("message", error.getMessage());             // 系统异常消息
+        failure.put("code", error.getCode());                   // 系统错误码
+        if (Objects.nonNull(error.getInfo())) {
+            // 业务数据信息，存在 info 时才提供
+            failure.put("info", error.getInfo());
+        }
         return failure;
     }
 }
