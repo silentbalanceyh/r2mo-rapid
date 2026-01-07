69fb852063ae8dbe4f99e12dbc8c50aca7092ef7
Lang <silentbalanceyh@126.com>
2026-01-07 09:41:07 +0800

    mvnd first

--- mvn-build.sh
+++ mvn-build.sh
@@ -1,2 +1,2 @@
 # export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
-mvn clean package install -Dquickly -DskipTests=true -Dmaven.javadoc.skip=true -Dmaven.compile.fork=true -T 16C
+mvnd clean install -Dquickly -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -Dmvnd.log.target=console
